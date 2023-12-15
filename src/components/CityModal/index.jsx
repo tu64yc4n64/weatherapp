6041635f13fe49426f487f55796fd0c3cc194494
tugaycan <tcduymaz94@gmail.com>
2023-12-15 16:56:18 +0300

    changed icons url

--- src/components/CityModal/index.jsx
+++ src/components/CityModal/index.jsx
@@ -1,6 +1,11 @@
 import Modal from 'react-modal';
 import { useSelector } from 'react-redux';
 import "./style.css"
+import thermometerpng from "../../assets/images/icons/thermometer.png"
+import temperaturepng from "../../assets/images/icons/temperature.png"
+import windypng from "../../assets/images/icons/windy.png"
+import sunrisepng from "../../assets/images/icons/sunrise.png"
+import moonrisepng from "../../assets/images/icons/moonrise.png"
 const CityModal = ({ cityName, isOpen, onClose }) => {
     const weatherSituation = useSelector(item => item.weather.weather)
     const sunset = useSelector(item => item.time.sunset)
@@ -18,7 +23,7 @@ const CityModal = ({ cityName, isOpen, onClose }) => {
                 <div>
                     <h2 className='text-[24px] font-[600] pb-[20px]'>{cityName} Hava Durumu</h2>
                     <div className='flex items-center pb-[15px]'>
-                        <img className='w-[34px] h-[34px] mr-[10px]' src="../../../public/images/icons/thermometer.png" alt="" />
+                        <img className='w-[34px] h-[34px] mr-[10px]' src={thermometerpng} alt="" />
                         <div>
                             <p className='font-[700] leading-[18px]'>{weatherSituation.main.temp}°C
                             </p>
@@ -27,7 +32,7 @@ const CityModal = ({ cityName, isOpen, onClose }) => {
                         </div>
                     </div>
                     <div className='flex items-center pb-[15px]'>
-                        <img className='w-[34px] h-[34px] mr-[10px]' src="../../../public/images/icons/temperature.png" alt="" />
+                        <img className='w-[34px] h-[34px] mr-[10px]' src={temperaturepng} alt="" />
                         <div>
                             <p className='font-[700]'>{weatherSituation.main.feels_like}°C
 
@@ -37,7 +42,7 @@ const CityModal = ({ cityName, isOpen, onClose }) => {
                         </div>
                     </div>
                     <div className='flex items-center pb-[15px]'>
-                        <img className='w-[34px] h-[34px] mr-[10px]' src="../../../public/images/icons/windy.png" alt="" />
+                        <img className='w-[34px] h-[34px] mr-[10px]' src={windypng} alt="" />
                         <div>
                             <p className='font-[700]'>{weatherSituation.wind.speed} m/s
 
@@ -46,7 +51,7 @@ const CityModal = ({ cityName, isOpen, onClose }) => {
                             <span className='text-[14px] font-[400]'> Rüzgar Hızı (m/s)</span>
                         </div>
                     </div><div className='flex items-center pb-[15px]'>
-                        <img className='w-[34px] h-[34px] mr-[10px]' src="../../../public/images/icons/sunrise.png" alt="" />
+                        <img className='w-[34px] h-[34px] mr-[10px]' src={sunrisepng} alt="" />
                         <div>
                             <p className='font-[700]'>{sunrise}
 
@@ -56,7 +61,7 @@ const CityModal = ({ cityName, isOpen, onClose }) => {
                         </div>
                     </div>
                     <div className='flex items-center pb-[15px]'>
-                        <img className='w-[34px] h-[34px] mr-[10px]' src="../../../public/images/icons/moonrise.png" alt="" />
+                        <img className='w-[34px] h-[34px] mr-[10px]' src={moonrisepng} alt="" />
                         <div>
                             <p className='font-[700]'>{sunset}
 
